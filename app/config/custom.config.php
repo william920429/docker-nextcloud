9bf66cadcbf29e93a64a52837f618f52c90ff664
william920429 <william920429@gmail.com>
2025-10-10 01:34:52 +0800

    add cache path

--- app/config/custom.config.php
+++ app/config/custom.config.php
@@ -1,8 +1,9 @@
 <?php
 $CONFIG = array (  
   'logfile' => '/var/tmp/nextcloud.log',
-  'log_rotate_size' => 10485760,
+  'log_rotate_size' => 10 * 1024 * 1024,
   'logfile_audit' => '/var/tmp/audit.log',
+  'cache_path' => '/var/tmp/nextcloud_cache',
   'trashbin_retention_obligation' => 'auto, 30',
   'versions_retention_obligation' => 'auto, 30',
   'activity_expire_days' => 30,
