04acb529a615eb20af701b5154ec7a0ba414e542
william920429 <william920429@gmail.com>
2026-01-24 00:14:50 +0800

    add env check

--- image/prepare.sh
+++ image/prepare.sh
@@ -1,9 +1,46 @@
 #!/usr/bin/env bash
 set -eu
 
+ENV_OK=1
+check_env () {
+    if [ "$#" -eq "0" ]; then
+        return 1
+    fi
+
+    KEY="$1"
+    VALUE="$(printenv $1)"
+    if [ -z "${VALUE}" ]; then
+        echo "ENV ${KEY} not set"
+        ENV_OK=0
+    elif [ "${2:-}" == "sensitive" ]; then
+        echo "ENV ${KEY}=***REMOVED SENSITIVE VALUE***"
+    else
+        echo "ENV ${KEY}='${VALUE}'"
+    fi
+}
+
 if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
     # Run /entrypoint.sh logic
     export NEXTCLOUD_UPDATE=1
+    export NEXTCLOUD_INIT_HTACCESS=1
+
+    # Check environment variables
+    echo "Checking environment variables..."
+    check_env PUID
+    check_env PGID
+    check_env NEXTCLOUD_ADMIN_USER
+    check_env NEXTCLOUD_ADMIN_PASSWORD sensitive
+    check_env POSTGRES_DB
+    check_env POSTGRES_USER
+    check_env POSTGRES_PASSWORD sensitive
+    check_env POSTGRES_HOST
+    check_env REDIS_HOST
+    check_env OVERWRITECLIURL
+
+    if [ "${ENV_OK}" -eq "0" ]; then
+        echo "Some environment variables not set!"
+        exit 1
+    fi
 
     # Make VA-API accessible by group www-data
     if [ ! -f "/dev-dri-group-was-added" ] && [ -c "/dev/dri/renderD128" ]; then
@@ -18,12 +55,12 @@ if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
     # Change www-data:www-data to ${PUID}:${PGID}
     [ "$(id -u www-data)" -ne "${PUID}" ] && usermod  -o -u "${PUID}" www-data
     [ "$(id -g www-data)" -ne "${PGID}" ] && groupmod -o -g "${PGID}" www-data
-    echo "This container will run with PUID=${PUID}, PGID=${PGID}"
 
     for dir in /var/www/html /var/www/log /var/www/cache; do
         echo "Checking permissions for ${dir}..."
         find "${dir}" ! -user www-data -or ! -group www-data \
-            -exec chown www-data:www-data {} \;
+            -exec chown --no-dereference www-data:www-data {} \; \
+            -print
     done
     echo "All things prepared."
 fi
