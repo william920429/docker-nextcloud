a2f4067ce9c03470e7e679d965021b307d281954
william920429 <william920429@gmail.com>
2026-01-26 12:53:35 +0800

    fix PUID PGID, change graceful shutdown timeout 3 sec

--- image/prepare.sh
+++ image/prepare.sh
@@ -3,10 +3,6 @@ set -eu
 
 ENV_OK=1
 check_env () {
-    if [ "$#" -eq "0" ]; then
-        return 1
-    fi
-
     KEY="$1"
     VALUE="$(printenv $1)"
     if [ -z "${VALUE}" ]; then
@@ -20,16 +16,11 @@ check_env () {
 }
 
 if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
-    # Run /entrypoint.sh logic
-    export NEXTCLOUD_UPDATE=1
-    export NEXTCLOUD_INIT_HTACCESS=1
-
     # Check environment variables
     echo "Checking environment variables..."
-    check_env PUID
-    check_env PGID
     check_env NEXTCLOUD_ADMIN_USER
     check_env NEXTCLOUD_ADMIN_PASSWORD sensitive
+    check_env NEXTCLOUD_HOST
     check_env POSTGRES_DB
     check_env POSTGRES_USER
     check_env POSTGRES_PASSWORD sensitive
@@ -43,19 +34,20 @@ if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
         exit 1
     fi
 
-    # Make VA-API accessible by group www-data
-    if [ ! -f "/dev-dri-group-was-added" ] && [ -c "/dev/dri/renderD128" ]; then
-        # From https://memories.gallery/hw-transcoding/#docker-installations
-        GID="$(stat -c "%g" /dev/dri/renderD128)"
-        groupadd -g "$GID" render2 || true # sometimes this is needed
-        GROUP="$(getent group "$GID" | cut -d: -f1)"
-        usermod -aG "$GROUP" www-data
-        touch "/dev-dri-group-was-added"
+    # Change www-data:www-data to ${PUID}:${PGID}
+    export PUID=${PUID:-33}
+    if [ "$(id -u www-data)" -ne "${PUID}" ]; then
+        usermod -o -u "${PUID}" www-data
+    fi
+    export PGID=${PGID:-33}
+    if [ "$(id -g www-data)" -ne "${PGID}" ]; then
+        groupmod -o -g "${PGID}" www-data
     fi
 
-    # Change www-data:www-data to ${PUID}:${PGID}
-    [ "$(id -u www-data)" -ne "${PUID}" ] && usermod  -o -u "${PUID}" www-data
-    [ "$(id -g www-data)" -ne "${PGID}" ] && groupmod -o -g "${PGID}" www-data
+    # Make VA-API accessible by www-data
+    if [ -c "/dev/dri/renderD128" ]; then
+        chgrp www-data /dev/dri/renderD128
+    fi
 
     for dir in /var/www/html /var/www/cache; do
         echo "Checking permissions for ${dir}..."
@@ -65,7 +57,9 @@ if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
     done
     echo "All things prepared."
 
-    # Run official logic (install/upgrade/...)
+    # Run /entrypoint.sh logic (install/upgrade/...)
+    export NEXTCLOUD_UPDATE=1
+    export NEXTCLOUD_INIT_HTACCESS=1
     /entrypoint.sh true
 
     # Prepare notify_push
@@ -85,7 +79,7 @@ if [ "$1" == "/usr/bin/supervisord" ] && [ "$EUID" -eq "0" ]; then
     export DATABASE_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}
     export DATABASE_PREFIX=oc_
     export REDIS_URL=redis://${REDIS_HOST}
-    export NEXTCLOUD_URL=http://app
+    export NEXTCLOUD_URL=http://${NEXTCLOUD_HOST}
 
 fi
 
